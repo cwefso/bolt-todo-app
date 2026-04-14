4f4e8c5822216e24a6f293a9d5ac8824fc2ef523
Charles Wefso <61127453+cwefso@users.noreply.github.com>
2026-04-14 08:27:21 -0600

    Update nav link 'To Do' to 'Tasks'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
                 }
                 end
               >
-                To Do
+                Tasks
               </NavLink>
             </li>
             <li>
